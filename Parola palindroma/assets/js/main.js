ed3c575452abf60189d3a044288afbc13e856ff7
erosalpe <eros.alpegiani@gmail.com>
2024-01-15 15:02:36 +0100

    feat(Palindroma) completato

---
+++ Parola palindroma/assets/js/main.js
@@ -0,0 +1,136 @@
+let parolaUtente
+do{
+    parolaUtente = prompt("Inserisci una parola")
+} while (isNaN(parolaUtente ) === false);
+
+
+
+
+function isPalindrome(parola){
+    const arrayBase = Array.from(parola);
+    let x = arrayBase.length - 1;
+    let flag = true;
+
+    if (arrayBase.length % 2 == 0){
+
+        let middleValue = arrayBase.length / 2;
+
+        console.log(middleValue);
+
+        for (let i = 0; i < middleValue; i++ && flag === true){
+
+            if (arrayBase[i] === arrayBase[x] ){
+
+                console.log(arrayBase[i], arrayBase[x]);
+
+                x--;
+
+            } else {
+                flag = false;
+            }
+        }
+
+        if (flag === true){
+            document.writeln(`La parola ${arrayBase} è palindroma`)
+        } else {
+            document.writeln(`La parola ${arrayBase} non è palindroma`)
+        }
+
+    } else if (arrayBase.length % 2 !== 0){
+
+        let middleValue = ((arrayBase.length / 2) - 0.5);
+
+        console.log("il valore derivato da un numero dispari è:", middleValue);
+
+        for (let i = 0; i < middleValue; i++ && flag === true){
+            if (arrayBase[i] === arrayBase[x]){
+
+                console.log(arrayBase[i], arrayBase[x]);
+
+                x--;
+            } else {
+                flag = false;
+            }
+        }
+
+        if (flag === true){
+            document.writeln(`La parola ${arrayBase} è palindroma`)
+        } else {
+            document.writeln(`La parola ${arrayBase} non è palindroma`)
+        }
+    }    
+}
+
+isPalindrome(parolaUtente);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const arrayReverse = [];
+
+function wordReverse (parola){
+
+    const arrayBase = Array.from(parola);
+
+
+    let indiceArrayReverse = arrayBase.length - 1;
+
+    let letteraCorrente
+
+    for(let i = 0; i < arrayBase.length; i++){
+
+        //console.log(indiceArrayReverse);
+
+        //console.log(arrayBase[indiceArrayReverse]);
+
+        letteraCorrente = arrayBase[indiceArrayReverse];
+
+        arrayReverse.push(letteraCorrente);
+
+        indiceArrayReverse--;
+
+        //console.log(arrayPalindromo, arrayBase);
+    }
+
+    document.writeln(`<br><br>La parola base è ${arrayBase}<br><br>La parola specchiata è ${arrayReverse}`);
+
+}
+
+wordReverse(parolaUtente);
